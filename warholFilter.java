8b7f11c370aa789feae37ec5a673eb0dc72e0bc4
benjamin200029 <bade4943@Stu.raritanval.edu>
2019-11-22 15:02:52 -0500

    created warhol classes

---
+++ warholFilter.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class warholFilter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class warholFilter
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class warholFilter
+     */
+    public warholFilter()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
