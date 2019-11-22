d67520705a3622295db6f179d0da2cbb266a922b
benjamin200029 <bade4943@Stu.raritanval.edu>
2019-11-22 13:40:09 -0500

    added in Red, Blue and Green filters, but fixed Red

--- ImageViewer.java
+++ ImageViewer.java
@@ -255,6 +255,10 @@ public class ImageViewer
         filterList.add(new GrayScaleFilter("Grayscale"));
         filterList.add(new EdgeFilter("Edge Detection"));
         filterList.add(new FishEyeFilter("Fish Eye"));
+        filterList.add(new RedFilter("Red Channel Filter"));
+        filterList.add(new BlueFilter("Blue Channel Filter"));
+        filterList.add(new GreenFilter("Green Channel Filter"));
+
        
         return filterList;
     }
---
+++ RedFilter.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+/**
+ * An image filter to make the image a bit darker.
+ * 
+ * @author Michael Kölling and David J. Barnes.
+ * @version 1.0
+ */
+public class RedFilter extends Filter
+{
+    /**
+     * Constructor for objects of class DarkerFilter.
+     * @param name The name of the filter.
+     */
+    public RedFilter(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Apply this filter to an image.
+     * 
+     * @param  image  The image to be changed by this filter.
+     */
+    public void apply(OFImage image)
+    {
+        int height = image.getHeight();
+        int width = image.getWidth();
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+               Color pix = image.getPixel(x, y);
+                int red = pix.getRed();
+                if(red <= 127) {
+                    red = 255 - red;
+                }
+                image.setPixel(x, y, new Color(red,0,0));
+            }
+        }
+    }
+}
